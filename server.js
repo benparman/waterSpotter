de79c2c6ef1997c111af7137356b3bfa1e33ce71
Ben Parman <benparman@gmail.com>
2018-07-16 13:03:00 -0600

    Added conditional statement to server.js which fixed error with port 8080

--- server.js
+++ server.js
@@ -3,9 +3,12 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080, function() {
-  console.info(`Application is listening on ${this.address().port}`);
-});
+
+if (require.main === module) {
+  app.listen(process.env.PORT || 8080, function() {
+    console.info(`Application is listening on ${this.address().port}`);
+  });
+}
 
 
 module.exports = app;
