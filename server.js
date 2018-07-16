884c7d8574faa26a56cb88117ea31b9defebc953
Ben Parman <benparman@gmail.com>
2018-07-16 11:29:46 -0600

    changed port back to 8080

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 8080, function() {
   console.info(`Application is listening on ${this.address().port}`);
 });
 
