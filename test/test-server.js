25d43e9ee6db69e91e69bd1d78e8acfe1ca81f16
Ben Parman <benparman@gmail.com>
2018-07-18 18:20:50 -0600

    Added faker.js dependency in package.json, wrote functions to generate and seed temporary location data for tests in test-server.js.  Working on bundling tests within one describe function that will first invoke runServer(), seedLocationData(), tearDownDb(), and closeServer() before each test.

--- test/test-server.js
+++ test/test-server.js
@@ -2,12 +2,53 @@
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const {app} = require('../server.js');
-
-
+const faker = require('faker');
+const mongoose = require('mongoose');
 const expect = chai.expect;
+const {Location} = require('../models'); // ".."" moves up one level, out of test and into root
+const {app, runServer, closeServer} = require('../server');
+const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
+//------------------------------
+function seedLocationData() {
+  console.info('seeding location data');
+  const seedData = [];
+  for(let i =0; i<10; i++) {
+    seedData.push({
+      contributor: faker.name.firstName(),
+      coordinates: {
+        lat: faker.random.number(),
+        lon: faker.random.number()
+      },
+      date_added: faker.date.past(),
+      type: faker.random.word(),
+      verified: faker.random.boolean()
+    });
+  }
+  return Location.insertMany(seedData);
+}
+function generateLocationData() {
+  return {
+    contributor: faker.name.firstName(),
+    coordinates: {
+      lat: faker.random.number(),
+      lon: faker.random.number()
+    },
+    date_added: faker.date.past(),
+    type: faker.random.word(),
+    verified: faker.random.boolean()
+  };
+}
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+//------------------------------
 
 describe('HTTP requests to root directory', function(){
   it('should expose a html document in the \'public\' folder', function(){
@@ -24,6 +65,16 @@ describe('HTTP requests to root directory', function(){
       .get('/')
       .then(function(res){
         expect(res).to.have.status(200);
+      });
+  });
+});
+describe('GET requests made to \'/locations\' endpoint', function() {
+  it('should expose documents in the \'locations\' collection in the database', function() {
+    return chai
+      .request(app)
+      .get('/locations')
+      .then(function(res){
+        expect(res).to.be.json;
       });
   });
 });
