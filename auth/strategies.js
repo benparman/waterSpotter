099dfda720b0edd36d492a5b4784b73d433dc164
Ben Parman <benparman@gmail.com>
2018-07-28 10:26:19 -0600

    Found and fixed a few typos in auth/strategies.js.

--- auth/strategies.js
+++ auth/strategies.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Strategy: localStrategy } = require('passport-local');
+const { Strategy: LocalStrategy } = require('passport-local');
 
 //Use destructuring assignment to export to JwtStrategy
 const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
@@ -8,35 +8,35 @@ const {Strategy: JwtStrategy, ExtractJwt} = require('passport-jwt');
 const { User } = require('../users/models');
 const {JWT_SECRET } = require('../config');
 
-const localStrategy = new localStrategy((username, password, callback) => {
+const localStrategy = new LocalStrategy((username, password, callback) => {
   let user;
-  USER.findOne({ username: username})
-  .then(_user => {
-    user = _user;
-    if (!user) {
-      // Returns a rejected promise to exit .then chain
-      return Promise.reject({
-        reason: 'LoginError',
-        message: 'Incorrect username or password'
-      });
-    }
-    return user.validatePassword(password);
-  })
-  .then(isValid => {
-    if (!isValid) {
-      return Promise.reject({
-        reason: 'LoginError',
-        message: 'Incorrect username or password'
-      });
-    }
-    return callback(null, user);
-  })
-  .catch(err => {
-    if (err.reason === 'LoginError') {
-      return callback(null, false, err);
-    }
-    return callback(err, false);
-  });
+  User.findOne({ username: username})
+    .then(_user => {
+      user = _user;
+      if (!user) {
+        // Returns a rejected promise to exit .then chain
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+      return user.validatePassword(password);
+    })
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect username or password'
+        });
+      }
+      return callback(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return callback(null, false, err);
+      }
+      return callback(err, false);
+    });
 });
 
 const jwtStrategy = new JwtStrategy(
@@ -46,10 +46,10 @@ const jwtStrategy = new JwtStrategy(
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
     // Specifies that only HS256 tokens are permitted
     algorithm: ['HS256']
-},
-  (payLoad, done) => {
+  },
+  (payload, done) => {
     done(null, payload.user);
   }
-)
+);
 
 module.exports = { localStrategy, jwtStrategy };
