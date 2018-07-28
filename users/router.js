435581f19530d84424a6bf48c59418cac9885b0c
Ben Parman <benparman@gmail.com>
2018-07-28 11:16:08 -0600

    completed users/index.js, users/models/js, users/router.js

---
+++ users/router.js
@@ -0,0 +1,134 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const {User} = require('./models');
+const router = express.Router();
+const jsonParser = bodyParser.json();
+
+//POST - New user registration
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if(missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password', 'firstName', 'lastName'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected a string',
+      location: nonStringField
+    });
+  }
+
+  //Trims white space (if present) from ends ofusername and password, then to 
+  //see if trimmed username and password match non-trimmed username and password.
+  //If they do not match, error message is sent alerting that username and
+  //password must not begin or end with whitespace.
+  const explicitlyTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicitlyTrimmedFields.find(
+    field => req.body[field].trim() !== req.body
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 10,
+      max: 72
+    }
+  };
+
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+        req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+        req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField].min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField].max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  let {username, password, firstName = '', lastName = ''} = req.body;
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+
+  return User.find({username})
+    .count()
+    .then(count => {
+      if (count > 0) {
+        //This would indicate that username already exists
+        return Promise.reject({
+          code: 422,
+          reason: 'ValidationError',
+          message: 'Username already taken',
+          location: 'username'
+        });
+      }
+      // Hashes password if username does not already exist
+      return User.hashPassword(password);
+    })
+    .then(hash => {
+      return User.create({
+        username,
+        password: hash,
+        firstName,
+        lastName
+      });
+    })
+    .then(user => {
+      return res.status(201).json(user.serialize());
+    })
+    .catch(err => {
+      if (err.reason === 'ValidationError') {
+        return res.status(err.code).json(err);
+      }
+      res.status(500).json(
+        {
+          code: 500, 
+          message: 'Internal server error'});
+    });
+});
+
+//*************** REMOVE THIS BEFORE PRODUCTION *****************/
+//*************** DO NOT USE *REAL* PASSWORDS!! *****************/
+router.get('/', (req, res) => {
+  return User.find()
+    .then(users => res.json(users.map(user => user.serialize())))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+//*************** REMOVE THIS BEFORE PRODUCTION *****************/
+//*************** DO NOT USE *REAL* PASSWORDS!! *****************/
+
+module.exports = {router};
