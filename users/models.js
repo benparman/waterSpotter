c088f785c670c2a062e56fe7d9d23e0afd596874
Ben Parman <benparman@gmail.com>
2018-08-15 12:43:41 -0600

    Updated users/models.js UserSchema to specify firstName and lastName fields to be required.

--- users/models.js
+++ users/models.js
@@ -8,8 +8,8 @@ const UserSchema = mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    firstName: { type: String, default: '' },
-    lastName: { type: String, default: '' }
+    firstName: { type: String, default: '' , required: true},
+    lastName: { type: String, default: '' , required: true}
   }
 );
 
