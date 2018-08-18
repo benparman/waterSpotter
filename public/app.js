9a4cc6c1b387bc93fc79bb6efc583b5499449e79
Ben Parman <benparman@gmail.com>
2018-08-17 22:10:49 -0600

    Added promise to call initMap() again after initial pageload *IF* navigator.geolocation (user's phsyical location) is present.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,30 @@
 'use strict';
 
 const geoCodingEndpoint='https://maps.googleapis.com/maps/api/geocode/json';
-const currentLocation='40.541281, -105.126432';
+let currentLocation={lat: 40.543504, lng: -105.127969};
 
-function initMap() {
-  console.log('initMap ran on line 7');
-  var location = {lat: 40.543504, lng: -105.127969};
+let getGpsLocation = new Promise((resolve, reject) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      currentLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      resolve(console.log('resolved'), initMap(currentLocation));
+    });
+  }
+  else {
+    reject();
+  }
+});
+
+function initMap(coords) {
+  console.log('initMap ran');
+  var location = currentLocation;
   const mapOptions = {
     mapTypeId: 'terrain',
     zoom: 14,
-    center: location
+    center: coords
   };
   
   let map = new google.maps.Map(document.getElementById('map'), mapOptions);
@@ -21,12 +36,16 @@ function listen() {
   $('#js-location-submit-button').click(function(event) {
     console.log('did something');
     event.preventDefault();
-    initMap();
+    // initMap(currentLocation);
   });
+  // initMap();
 }
 
 
 $(window).on('load', function() {
+  initMap(currentLocation);
   listen();
-  initMap();
+  // initMap(currentLocation);
+  getGpsLocation
+    .then(console.log('resolved on 49'));
 });
