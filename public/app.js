f429278acfe9585667a23121b63db83ed31115ff
Ben Parman <benparman@gmail.com>
2018-08-18 16:12:10 -0600

    Put search box inside a form and changed event listener to watch for .submit instead of .click to enable hitting enter instead of only clicking the 'Go' button.  Updated classes and id's in index.html.  Changed app.js to prompt for current location on button click instead of on page load, and wrapped promise that returns user's location inside a function and made the promise anonymous.

--- public/app.js
+++ public/app.js
@@ -1,23 +1,24 @@
 'use strict';
-
 const geoCodingEndpoint='https://maps.googleapis.com/maps/api/geocode/json';
 const geoCodingApiKey='AIzaSyB05Gh-VXpXhypmBg4R3hzZl8zFxJJYLGQ';
 let currentLocation={lat: 40.543504, lng: -105.127969};
 
-let getGpsLocation = new Promise((resolve, reject) => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      currentLocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      resolve(console.log('resolved'), initMap(currentLocation));
-    });
-  }
-  else {
-    reject();
-  }
-});
+function getLocation() {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        currentLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+        resolve( initMap(currentLocation) );
+      });
+    }
+    else {
+      reject();
+    }
+  });
+}
 
 function geoCodeLocation(location) {
   const settings = {
@@ -39,30 +40,29 @@ function geoCodeLocation(location) {
 }
 
 function initMap(coords) {
-  console.log('initMap ran');
   var location = currentLocation;
   const mapOptions = {
     mapTypeId: 'terrain',
     zoom: 14,
     center: coords
   };
-  
   let map = new google.maps.Map(document.getElementById('map'), mapOptions);
   // let marker = new google.maps.Marker({position: location, map: map});
   $('#map').show();
 }
 
 function listen() {
-  $('#js-location-submit-button').click(function(event) {
-    console.log('did something');
+  $('#searchLocation').submit(function(event) {
     event.preventDefault();
-    geoCodeLocation($('#searchLocation').val() || currentLocation);
+    geoCodeLocation($('.searchTerms').val());
+  });
+  $('#myLocation-button').click(function(event){
+    event.preventDefault();
+    getLocation();
   });
 }
 
 $(window).on('load', function() {
   initMap(currentLocation);
   listen();
-  getGpsLocation
-    .then(console.log('resolved on 49'));
 });
