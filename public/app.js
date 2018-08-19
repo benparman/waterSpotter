05e9508162a63638a69873b9e8c7a22ea51e9c18
Ben Parman <benparman@gmail.com>
2018-08-18 23:51:07 -0600

    Added a mockData{ } object to app.js with some dummy locations to use for testing ability to add markers to map.  Wrote a new function called addMarkersToMap(locations, map) in which the locations argument represents the location data the function then sorts through to create the marker data, and map represents the google map generated by the initMap() function.  Everything is currently working correctly, but need to refactor to obtain data from database instead of the mockData object.

--- public/app.js
+++ public/app.js
@@ -3,6 +3,78 @@ const geoCodingEndpoint='https://maps.googleapis.com/maps/api/geocode/json';
 const geoCodingApiKey='AIzaSyB05Gh-VXpXhypmBg4R3hzZl8zFxJJYLGQ';
 let currentLocation={lat: 40.543504, lng: -105.127969};
 
+let mockData = {
+  'locations': [
+    {
+      'title': 'Lakeside Fire Station',
+      'description': 'Faucet on North side of building',
+      'contributor': 'John Doe',
+      'coordinates': {
+        'lat': 40.531654,
+        'lon': -105.162646
+      },
+      'date_added': '2018-06-15',
+      'id': 11111111,
+      'type': 'Faucet',
+      'verified': false
+    },
+    {
+      'title': 'Mulholland Canyon Ranch',
+      'description': 'Spigot next to gate at end of driveway',
+      'contributor': 'Billy Bob',
+      'coordinates': {
+        'lat': 40.58,
+        'lon': -105.16
+      },
+      'date_added': '2018-05-15',
+      'id': 11111111,
+      'type': 'Spigot',
+      'verified': false
+    },
+    {
+      'title': 'Monarch Crest Spring',
+      'description': 'Small pipe with natural running spring water on east side of trail',
+      'contributor': 'Sally Stranger',
+      'coordinates': {
+        'lat': 40.55,
+        'lon': -105.17
+      },
+      'date_added': '2018-07-15',
+      'id': 11111111,
+      'type': 'Spring',
+      'verified': false
+    },
+    {
+      'title': 'Salida Downtown Public Restrooms',
+      'description': '',
+      'contributor': 'Franklin Roosevelt',
+      'coordinates': {
+        'lat': 40.53,
+        'lon': -105.16
+      },
+      'date_added': '2018-07-13',
+      'id': 11111111,
+      'type': 'Drinking Fountain',
+      'verified': false
+    },
+    {
+      'title': 'Lakeside Fire Station',
+      'description': '',
+      'contributor': 'Albert Einstein',
+      'coordinates': {
+        'lat': 40.535,
+        'lon': -105.168
+      },
+      'date_added': '2018-07-15',
+      'id': 11111111,
+      'type': 'natural spring',
+      'verified': false
+    }
+  ]
+};
+
+console.log('This is the mock data: ', mockData);
+
 function getLocation() {
   return new Promise((resolve, reject) => {
     if (navigator.geolocation) {
@@ -39,15 +111,35 @@ function geoCodeLocation(location) {
   $.ajax(settings);
 }
 
+function addMarkersToMap(locations, map) {
+  // mapMarkers array is only to needed for logging purposes!
+  let mapMarkers = [];
+  locations.locations.forEach(function(storedPlace) {
+    const marker = new google.maps.Marker({
+      position: {
+        lat: storedPlace.coordinates.lat,
+        lng: storedPlace.coordinates.lat
+      },
+      title: storedPlace.title,
+      map: map
+    });
+    // not needed - only here to console.log below
+    mapMarkers.push(marker);
+    return marker;
+  });
+  // this is here to visually inspect data, not necessary
+  console.log('These are the map markers :', mapMarkers);
+}
+
 function initMap(coords) {
-  var location = currentLocation;
+  // var location = currentLocation;
   const mapOptions = {
     mapTypeId: 'terrain',
     zoom: 14,
     center: coords
   };
   let map = new google.maps.Map(document.getElementById('map'), mapOptions);
-  // let marker = new google.maps.Marker({position: location, map: map});
+  addMarkersToMap(mockData, map);
   $('#map').show();
 }
 
---
+++ public/mockData.js
@@ -0,0 +1,73 @@
+'use strict';
+
+let mockData = {
+  'locations': [
+    {
+      'title': 'Lakeside Fire Station',
+      'description': 'Faucet on North side of building',
+      'contributor': 'John Doe',
+      'coordinates': {
+        'lat': 40.531654,
+        'lon': -105.162646
+      },
+      'date_added': '2018-06-15',
+      'id': 11111111,
+      'type': 'Faucet',
+      'verified': false
+    },
+    {
+      'title': 'Mulholland Canyon Ranch',
+      'description': 'Spigot next to gate at end of driveway',
+      'contributor': 'Billy Bob',
+      'coordinates': {
+        'lat': 40.58,
+        'lon': -105.16
+      },
+      'date_added': '2018-05-15',
+      'id': 11111111,
+      'type': 'Spigot',
+      'verified': false
+    },
+    {
+      'title': 'Monarch Crest Spring',
+      'description': 'Small pipe with natural running spring water on east side of trail',
+      'contributor': 'Sally Stranger',
+      'coordinates': {
+        'lat': 40.55,
+        'lon': -105.17
+      },
+      'date_added': '2018-07-15',
+      'id': 11111111,
+      'type': 'Spring',
+      'verified': false
+    },
+    {
+      'title': 'Salida Downtown Public Restrooms',
+      'description': '',
+      'contributor': 'Franklin Roosevelt',
+      'coordinates': {
+        'lat': 40.53,
+        'lon': -105.16
+      },
+      'date_added': '2018-07-13',
+      'id': 11111111,
+      'type': 'Drinking Fountain',
+      'verified': false
+    },
+    {
+      'title': 'Lakeside Fire Station',
+      'description': '',
+      'contributor': 'Albert Einstein',
+      'coordinates': {
+        'lat': 40.535,
+        'lon': -105.168
+      },
+      'date_added': '2018-07-15',
+      'id': 11111111,
+      'type': 'natural spring',
+      'verified': false
+    }
+  ]
+};
+
+module.exports = {mockData};
