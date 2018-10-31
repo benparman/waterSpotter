a8ccd0bf4ffec27b16eb877db29af71968fd07c2
Ben Parman <benparman@gmail.com>
2018-10-30 19:11:29 -0600

    Further normalized CSS styles for cross-page consistency.  Changed google map style and relocated control buttons.

--- public/app.js
+++ public/app.js
@@ -96,7 +96,105 @@ function initMap() {
   const mapOptions = {
     mapTypeId: 'terrain',
     zoom: 14,
-    center: STATE.defaultLocation
+    fullscreenControl: false,
+    center: STATE.defaultLocation,
+    styles: [
+      {
+          "featureType": "all",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#ebebeb"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape.man_made",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#d6d2cc"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#8d867c"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#8b1b41"
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "color": "#8b1b41"
+              },
+              {
+                  "lightness": "50"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#fcd27f"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "geometry.stroke",
+          "stylers": [
+              {
+                  "color": "#fcd27f"
+              },
+              {
+                  "lightness": "50"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "geometry.fill",
+          "stylers": [
+              {
+                  "color": "#12202f"
+              },
+              {
+                  "gamma": "2.00"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "lightness": "100"
+              }
+          ]
+      }
+  ],
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      mapTypeIds: ['terrain', 'satellite'],
+      position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
   };
   STATE.map = new google.maps.Map(document.getElementById('map'), mapOptions);
   addMarkersToMap();
