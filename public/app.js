35ce4a2f97c5f7e9543926b4759213a70aea89dc
Ben Parman <benparman@gmail.com>
2018-11-13 13:42:09 -0700

    Add content to 'About' and set styles.

--- public/app.js
+++ public/app.js
@@ -621,6 +621,29 @@ $(window).on('load', function() {
     $('#map').css('pointer-events', 'none');
   });
 
+  //------
+  $('#about').on('click', function(event) {
+    event.preventDefault();
+    $('.js-user-form').html(`
+      <div class="about">
+        <p class = "aboutText">HydroMapper is a tool for finding and sharing public drinking water resources for cyclists, runners, travelers, or anyone else in need of a refill.</p>
+        <p class = "aboutText">The map displays existing user added resources with unique map marker icons indicating the resource type, such as drinking fountains, spigots, natural springs, etc.  Resource details can be shown by simply clicking on one of the markers.</p>
+        <p class = "aboutText">The map center can be changed by using the search box or the “My Location” button at the top of the screen.  The search box will accept very specific to very general search terms, from zip codes to states, give it try!  The ‘My Location’ button relies on availability of GPS or network location, and may not be available depending on your device and connection type.</p>
+        <p class = "aboutText">The map can also be moved by dragging the map with your mouse cursor, or by touch on a mobile device.  The map is capable of showing Terrain or Satellite views, which can be changed by clicking the respective button at the bottom left of the map.  You may zoom in and out using the “+” and “-“ buttons at the bottom right of the screen, and by dragging the icon of the yellow person onto the screen, you may see the “Street View” of a location, if available.</p>
+        <p class = "aboutText">If you would like to add a resource to the map, you may do so by creating an account by using the “Sign Up” link in the top right, and logging in (also a link in the top right)  which will enable a “New” button next to the “My Location” button at the top of the screen.  A unique user account is required to add new resources, and provides the ability to later edit or remove resources that an individual user added.</p>
+        <p class = "aboutText">This application depends on input from users like you!  Please consider creating an account and adding reliable locations to the map that may be useful to other users.</p>
+        <p class = "aboutText">Please direct any questions, comments, or concerns to ***add-address***@gmail.com</p>
+        <button class="close-logIn-signIn-form" type="button">Close</button>
+      </div>
+      `
+    );
+    $('.js-user-form').show();
+    $('#map').css('opacity', .4);
+    $('#map').css('pointer-events', 'none');
+  });
+  //------
+
+
   $('.js-user-form').on('click', '.close-logIn-signIn-form',function(event) {
     event.preventDefault();
     $('.js-user-form').hide();
