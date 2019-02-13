e21e356ac8eca3ffae73d0cc34ec0dca4c99fd71
Ben Parman <benparman@gmail.com>
2019-02-12 21:02:23 -0700

    Update styles for email links

--- public/app.js
+++ public/app.js
@@ -633,7 +633,7 @@ $(window).on('load', function() {
         <p class = "aboutText">The map can also be moved by dragging the map with your mouse cursor, or by touch on a mobile device.  The map is capable of showing Terrain or Satellite views, which can be changed by clicking the respective button at the bottom left of the map.  You may zoom in and out using the “+” and “-“ buttons at the bottom right of the screen, and by dragging the icon of the yellow person onto the screen, you may see the “Street View” of a location, if available.</p>
         <p class = "aboutText">If you would like to add a resource to the map, you may do so by creating an account by using the “Sign Up” link in the top right, and logging in (also a link in the top right)  which will enable a “New” button next to the “My Location” button at the top of the screen.  A unique user account is required to add new resources, and provides the ability to later edit or remove resources that an individual user added.</p>
         <p class = "aboutText">This application depends on input from users like you!  Please consider creating an account and adding reliable locations to the map that may be useful to other users.</p>
-        <p class = "aboutText">Please direct any questions, comments, or concerns to benparman@gmail.com</p>
+        <p class = "aboutText">Please direct any questions, comments, or concerns to <a href="mailto:benparman@gmail.com">benparman@gmail.com</a></p></p>
         </div>
         <button class="closeUserForm" id="closeAbout" type="button">Close</button>
       </div>
@@ -649,7 +649,7 @@ $(window).on('load', function() {
     $('.js-user-form').html(`
       <div class="about">
       <div class="aboutText">
-        <p class = "aboutText">Please direct any questions, comments, or concerns to benparman@gmail.com</p>
+        <p class = "aboutText">Please direct any questions, comments, or concerns to <a href="mailto:benparman@gmail.com">benparman@gmail.com</a></p>
         </div>
         <button class="closeUserForm" id="closeAbout" type="button">Close</button>
       </div>
